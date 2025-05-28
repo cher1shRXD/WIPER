68bd2913b11510c7ee7e4d9ab4fb2da6ac4cb062
김태우 <taewoo080401@gmail.com>
2025-05-28 22:26:11 +0900

    Initial Commit

---
+++ Wiper/WiperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiperApp.swift
+//  Wiper
+//
+//  Created by cher1shRXD on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct WiperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
